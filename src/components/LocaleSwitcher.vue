f952719e4d014b37b32582b7965c22c6c7dc4101
Aleksandr Podoliak <aleksandr.podoljak@gmail.com>
2022-09-21 20:55:52 +0300

    build

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- v-model="$i18n.locale" -->
+  <select
+    :value="$i18n.locale"
+    @change="switchLanguage($event)"
+  >
+    <option v-for="(locale, i) in locales" :key="`locale-${i}`" :value="locale.value">
+      {{ locale.text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "LocaleSwitcher",
+  data() {
+    return { 
+      locales: [
+        {
+          value: 'uk',
+          text: 'Українська',
+        },
+        {
+          value: 'en',
+          text: 'English',
+        }, 
+        {
+          value: 'ru',
+          text: 'Русский',
+        },
+      ] 
+    };
+  },
+  methods: {
+    switchLanguage(event) {
+      this.$i18n.locale = event.target.value;
+      this.$router.replace(`/${event.target.value}`)
+    },
+  },
+};
+</script>
---
+++ src/views/mainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    {{ $t('message') }}
+  </div>
+  
+  <LocaleSwitcher />
+</template>
+
+<script>
+
+import LocaleSwitcher from '../components/LocaleSwitcher';
+
+export default {
+
+  name: 'mainPage',
+  components: {
+    LocaleSwitcher,
+  },
+
+}
+</script>
